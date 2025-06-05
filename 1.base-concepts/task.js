4bef1ad485b53e502df10f8b2e28e2ee1aa1d63d
Marina Gorina <squalodivertente@gmail.com>
2025-06-05 15:12:51 +0300

    Task v1.0

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,22 @@
 "use strict"
-function solveEquation(a, b, c) {
+function solveEquation(a,b,c) {
+  let d = b**-4*a*c;
   let arr = [];
-  
+  if (d < 0) {
+    arr = [];
+  }
+  else if (d === 0) {
+    arr = [-b/(2*a)];
+  } 
+  else {
+    arr = [(-b + Math.sqrt(d) )/(2*a),(-b - Math.sqrt(d) )/(2*a)];
+  }
   return arr;
 }
+
+console.log(solveEquation(1,5,4))
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
