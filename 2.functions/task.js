5e0a9003524303686a046b10d999f2f329a54f88
Marina Gorina <squalodivertente@gmail.com>
2025-06-07 23:16:51 +0300

    Task #2 v1.0

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,19 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  };
+  let sum = arr.reduce(function (a,b) {
+    return a+b;
+  });
+  return { min: min, max: max, avg: parseFloat((sum/(arr.length)).toFixed(2)) };
 }
+
 
 function summElementsWorker(...arr) {
 
